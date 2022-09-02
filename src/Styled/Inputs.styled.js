6b0ef7a39d9128b5f24cec41fe175a92afdb722d
realkevinbrian <realkevinbrian@gmail.com>
2022-09-02 11:39:03 +0200

    #NEW COMPONENT
    Added Inputs Styled Component

---
+++ src/Styled/Inputs.styled.js
@@ -0,0 +1,24 @@
+/****
+ * Inputs Styled Components for reusability and Performance
+ */
+
+import styled from "styled-components";
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const InputLabel = styled.label`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const InputText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  outline: none;
+  border: 1px solid transparent;
+  background-color: transparent;
+`;
