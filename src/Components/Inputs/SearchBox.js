59aa4d05b07278ab843291eef5ba268c31473c2f
realkevinbrian <realkevinbrian@gmail.com>
2022-09-02 17:24:18 +0200

    Added SearchBox Component
     #NEW FEATURE

---
+++ src/Components/Inputs/SearchBox.js
@@ -0,0 +1,17 @@
+import { Search } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
+import { InputText, SearchBoxContainer } from "../../Styled/Inputs.styled";
+
+function SearchBox() {
+  return (
+    <SearchBoxContainer>
+      <InputText type="search" placeholder="Pesquise por nome" />
+      <IconButton>
+        <Search />
+      </IconButton>
+    </SearchBoxContainer>
+  );
+}
+
+export default SearchBox;
--- src/Styled/Inputs.styled.js
+++ src/Styled/Inputs.styled.js
@@ -8,10 +8,9 @@ export const InputGroup = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  border: 1px solid ${({theme})=>theme.colors.cleanGrey};
-  gap: .4rem;
+  border: 1px solid ${({ theme }) => theme.colors.cleanGrey};
+  gap: 0.4rem;
   padding: 1px 10px;
-
 `;
 export const InputLabel = styled.label`
   display: flex;
@@ -26,4 +25,23 @@ export const InputText = styled.input`
   outline: none;
   border: 1px solid transparent;
   background-color: transparent;
+`;
+
+/***
+ * SearchBox
+ */
+
+export const SearchBoxContainer = styled.div`
+  border: 1px solid;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+
+  button {
+    background-color: ${({ theme }) => theme.colors.raisinBlack};
+    background-color: ${({ theme }) => theme.colors.white};
+    
+  }
 `;
