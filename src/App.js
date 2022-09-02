7b40efcae8476d6aa156e9c0bdd7c719bd2e0efc
realkevinbrian <realkevinbrian@gmail.com>
2022-09-02 20:23:46 +0200

    Added React Router Library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import SideMenu from "./Components/AppBar/SideBar";
-import Topbar from "./Components/AppBar/Topbar";
-import Login from "./Pages/Login";
+import SearchBox from "./Components/Inputs/SearchBox";
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
-      <SideMenu />
     </div>
   );
 }
